6c52fddd519ef2cbfddb8de490c08ab374393787
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-07-04 13:21:12 +0800

    env update

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,7 +1,5 @@
 import { BrevoClient } from "@getbrevo/brevo";
 
-console.log("BREVO KEY loaded:", process.env.BREVO_API_KEY ? process.env.BREVO_API_KEY.slice(0, 12) + "..." : "UNDEFINED");
-
 const brevo = new BrevoClient({
   apiKey: process.env.BREVO_API_KEY,
 });
